550792dc85864fe903dbb6223c1301cac1ebc2aa
s1rcody66 <cody200600@gmail.com>
2024-10-10 19:43:12 +0300

    completelab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( {
+            name,
+            email
+        })  
+    })
+    .then(res=>res.json())
+    .then(obj=>document.body.innerHTML = obj["id"])
+    .catch(error=>document.body.innerHTML = error.message)
+   }
+    
